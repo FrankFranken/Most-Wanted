8d39cd283c5159a51eb8eb07b02f854e977852d7
FrankFranken <Frankfranken1985@gmail.com>
2017-01-24 16:55:50 -0600

    username code finished

--- gui.js
+++ gui.js
@@ -1,14 +1,49 @@
 function initSearch(people){
+	var search = prompt("Do you know the name you'd like to search?")
+	switch(search){
+		case "yes":
+			getName(people);
+			break;
+		case "no":
+			getAge();
+			break;
+		default:
+			alert("please make valid selection: yes or no");
+		initSearch(people);	
+	}
+}
+function getName(people){
+	var userName = prompt("enter a first name")
+	var userlastName = prompt("enter a last name")
+	findName(people, userName, userlastName)
+}
+function findName(people, userName, userlastName){	
+	var searchName = people.filter(function(person){
+        if(person.firstName.toLowerCase() == userName.toLowerCase() || person.lastName.toLowerCase() == userName.toLowerCase()){
+            return true
+        }
+        else{
+            return false
+        }
+    });	
+    for (var i = 0; i < searchName.length; i++) {
+        displayResults(searchName, this.i)
+    }
+}
+function descendent(person, people, descendents=[]){
 	;
 }
-function initSearchByName(){
+function nextkin(){
 	;
 }
-function initSearchByTraits(){
+function family(){
 	;
 }
-function displayResults(){
+function initSearchByTrait(){
 	;
+}
+function displayResults(searchName, i, people){
+	alert(people[i]["firstName"] + "" + people[i]["lastName"]);	
 }
 function isNumeric() {
 	;
