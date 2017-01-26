7f25f4f12dd2e99a55e949b60b4c11b906eb4640
FrankFranken <Frankfranken1985@gmail.com>
2017-01-26 17:43:19 -0600

    Descendents functions

--- gui.js
+++ gui.js
@@ -1,5 +1,5 @@
 function initSearch(people){
-	var search = prompt("Do you know the name you'd like to search?")
+	var search = prompt("Do you know the name you'd like to search?");
 	switch(search){
 		case "yes":
 			getName(people);
@@ -11,60 +11,6 @@ function initSearch(people){
 			alert("please make valid selection: yes or no");
 		initSearch(people);
 	}
-}
-function getName(people){
-	var userName = prompt("enter a first name")
-	var userlastName = prompt("enter a last name")
-	findName(people, userName, userlastName)
-}
-function findName(people, userName, userlastName){
-	var searchName = people.filter(function(person){
-        if(person.firstName.toLowerCase() === userName.toLowerCase() || person.lastName.toLowerCase() === userName.toLowerCase()){
-            return true
-        }
-        else{
-            return false
-        }
-    });
-    printSearchName(people, searchName);
-    // for (var i = 0; i > searchName.Length; i++) {
-  	// printSearchName[i](people, searchName);
-    // }
-}
-function printSearchName(people, searchName){
-	alert("Match found \nName: " + searchName[0].firstName + " " + searchName[0].lastName +"\nGender: " + searchName[0].gender + "\nOccupation: " + searchName[0].occupation);
-}
-function getAge(people){
-	var Userage = prompt("Please enter the users age");
-	findAge(people, age);
-}
-function convertAge(people, Userage){
-	
-}
-function findAge(people, userAge){
-	var searchAge = people.filter(function(person){
-        if(person.firstname.toLowerCase() === userName.toLowerCase() || person.lastName.toLowerCase() === userName.toLowerCase()){
-            return true
-        }
-        else{
-            return false
-        }
-    });
-}
-function printAge(people){
-
-}
-function displayResults(searchName, i, people){
-
-}
-function descendent(person, people, descendents=[]){
-	;
-}
-function nextkin(){
-	;
-}
-function family(){
-	;
 }
 function initSearchByTrait(people){
 		var search = prompt("Would you like to search by height, weight, or eyecolor?");
@@ -78,47 +24,141 @@ function initSearchByTrait(people){
 			case "eyecolor":
 			getEyeColor(people);
 			break;
+			case "age":
+			getAge(people);
+			break;
+			case "occupation":
+			getOccupation(people);
+			break;
 			default:
-			alert("Please enter height, weight, or eyecolor")
+			alert("Please enter height, weight, or eyecolor");
 			initSearchByTrait(people);
 		}
 }
-function isNumeric() {
-	;
+
+
+
+
+function getName(people){
+	var userName = prompt("enter a first name");
+	var userlastName = prompt("enter a last name");
+	findName(people, userName, userlastName);
 }
-function setHeight(people){
-	var convertedHeight=[] = enterHeight / 12;
+
+function findName(people, userName, userlastName){
+	var searchName = people.filter(function(person){
+        if(person.firstName.toLowerCase() === userName.toLowerCase() || person.lastName.toLowerCase() === userName.toLowerCase()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
+    printSearchName(people, searchName);
+}
+function printSearchName(people, searchName){
+	alert("Match found \nName: " + searchName[0].firstName + " " + searchName[0].lastName +"\nGender: " + searchName[0].gender + "\nOccupation: " + searchName[0].occupation);
+	var decendentsChoice = prompt("Would your like to see this persons decendents? ");
+	switch(decendentsChoice){
+	case "yes":
+  descendents(people, searchName);
+	break;
+	case "no":
+	initSearch(people);
+	break;
+	default:
+	alert("please enter a valid selection");
+	break;
+}
+}
+
+
+function descendents(people, searchName){
+var nameID = searchName[0].id;
+findDescendents(people, nameID);
+}
+function findDescendents(people, nameID){
+	var filterDecendents = people.filter(function(person){
+        if(person.id === nameID()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
+}
+
+
+
+function getAge(people){
+	var Userage = prompt("Please enter the users age");
+	findAge(people, age);
+}
+function convertAge(people, Userage, tempAge=[]){
+	var today = new Date();
+		var dob = new Date();
+		var age = today.getFullYear() - dob.getFullYear();
+		var m = today.getMonth() - dob.getMonth();
+		if (m < 0 || m === 0 && today.getDate() < dob.getDate())
+		{
+				age--;
+		}
+		return age;
+}
+function findAge(people, userAge){
+	var searchAge = people.filter(function(person){
+        if(person.dob.tostring() === userAge.toLowerCase() || person.dob.toLowerCase() === userAge.toLowerCase()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
+}
+function printAge(people){
+
+}
+
+
+
+
+function displayResults(searchName, i, people){
+
+}
+
+function nextkin(){
+
+}
+function family(){
+
+}
+
+function isNumeric() {
 
 }
 function getHeight(people){
-	var enterHeight = prompt("Please enter a height")
-	setHeight(people, enterHeight)
+	var enterHeight = prompt("Please enter a height");
+	setHeight(people, enterHeight);
+}
+function setHeight(people, enterHeight){
+	var convertedHeight=[] = enterHeight / 12;
 }
 function findHeight(people, enterHeight){
 	var searchHeight = people.filter(function(person){
 			if(person.firstname.toLowerCase() === userName.toLowerCase() || person.lastName.toLowerCase() === userName.toLowerCase()){
-					return true
+					return true;
 			}
 			else{
-					return false
+					return false;
 			}
 	});
 	}
 function getWeight(){
-	;
+
 }
 function getEye(){
-	;
+
 }
 function getOccupation(){
-	var today = new Date();
-    var birthDate = new Date(dateString);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate()))
-    {
-        age--;
-    }
-    return age;
-	;
+
 }
