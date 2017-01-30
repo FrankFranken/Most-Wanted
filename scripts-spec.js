c3665b24774cc786ea26300a5701c38305772712
FrankFranken <Frankfranken1985@gmail.com>
2017-01-30 05:11:48 -0600

    deleted test files

--- scripts-spec.js
+++
@@ -1,5 +0,0 @@
-describe('function addSixTo', function(){
-  it('add 6 to 2', function() {
-    expect(addSixTo(4)).toEqual(10);
-  });
-});
--- scripts.js
+++
@@ -1,3 +0,0 @@
-function addSixTo(x){
-  return x + 6;
-}
